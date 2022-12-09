e07ba3544df7effa9e3746be90ef2ba2a651b2a8
Im Jayyyy <106723720+jayyy951@users.noreply.github.com>
2022-12-09 14:41:36 +0700

    udpate

--- spam.py
+++ spam.py
@@ -2,15 +2,15 @@ import requests
 import colorama
 from colorama import Fore,Back,Style
 
-def check(email,password):
+def check(input_15,input_17):
 
     headers = {
-        'authority': 'dalam.cbgml.com',
+        'authority': 'form.jotform.com',
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'accept-language': 'en-US,en;q=0.9',
         'cache-control': 'max-age=0',
-        'origin': 'https://thebiz.link/akulaku-konfirmasi',
-        'referer': 'https://thebiz.link/akulaku-konfirmasi/',
+        'origin': 'https://form.jotform.com/223400554197453',
+        'referer': 'https://form.jotform.com/223400554197453/',
         'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"',
@@ -22,13 +22,13 @@ def check(email,password):
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
     }
 
-    data2 = {
-        'email': email,
-        'password': password,
-        'login': 'Facebook',
+    data = {
+        'email': input_15,
+        'password': input_17,
+        'login': 'akulaku',
     }
 
-    response = requests.post('https://form.jotform.com/223400554197453', headers=headers, data2=data2)
+    response = requests.post('https://form.jotform.com/223400554197453', headers=headers, data=data)
     if response.status_code == 200:
         print (Fore.GREEN+Style.BRIGHT+'MAMPUSS!!!')
     else:
